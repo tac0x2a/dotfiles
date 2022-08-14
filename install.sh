1e7fc6d98fe966cfeb03209f1e341579fe069d34
TAC <tac@tac42.net>
2022-08-14 10:51:13 +0900

    add moreutils

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ sudo apt install -y \
 }
 
 # brew packages
-[[ -d /home/linuxbrew/ ]] && brew install bat exa q
+[[ -d /home/linuxbrew/ ]] && brew install bat exa q moreutils
 
 # rbenv -----------------------------------------------------------------------------------
 [[ -d ~/.rbenv ]] || {
