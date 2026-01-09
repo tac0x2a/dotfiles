088634904acb718bbbe26ac73979e7159e84b2cd
tac <tac@Lucy.local>
2026-01-10 00:03:32 +0900

    change: install.sh に合わせる

--- clean.sh
+++ clean.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ "$(uname)" == 'Darwin' ]; then OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then OS='Linux'
+else
+  echo "Unknown uname '$(uname)'. Exit."
+  exit -1
+fi;
+
+echo "### Cleanup for ${OS} ###"
+
 stow -v -d "$HOME/.dotfiles" -t "$HOME" -D .home_base
-if [ "$(uname)" == 'Darwin' ]; then
+[[ $OS == "Mac" ]] && {
   stow -v -d "$HOME/.dotfiles" -t "$HOME" -D .home_mac
-fi
+}
 
 rm -rf "$HOME/.zcompdump" "$HOME/.zcompdump.zwc" "$HOME/.local/share/sheldon"
 # rm -rf .dotfiles
